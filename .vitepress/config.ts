086100146befa79d06704dbb09849f875b9fadc7
Doc077 <dom.rajkowski@gmail.com>
2022-09-15 20:56:35 +0200

    Set up project

---
+++ .vitepress/config.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'Norther',
+  description: 'Modern, declarative framework for Node.js',
+  themeConfig: {
+    siteTitle: 'Norther',
+    logo: '/logo.png',
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/' },
+        ]
+      },
+    ],
+  }
+}
