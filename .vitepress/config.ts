46e337bdba429eaa4e223b294db32469a4d9eeee
Doc077 <dom.rajkowski@gmail.com>
2022-09-16 20:16:40 +0200

    Add home page and customize theme

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,11 +1,30 @@
 export default {
+  outDir: './dist',
   title: 'Norther',
   lang: 'en-US',
   description: 'Modern, declarative framework for Node.js',
+  head: [
+    [
+      'link', {
+        rel: 'icon',
+        href: '/logo.png',
+      },
+    ],
+  ],
   themeConfig: {
     siteTitle: 'Norther',
     logo: '/logo.png',
     outlineTitle: 'Table of Contents',
+    nav: [
+      { text: 'Docs', link: '/docs/introduction/northerjs-framework.html' },
+      { text: 'Github', link: 'https://github.com/northerjs/norther' },
+    ],
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/northerjs/norther',
+      },
+    ],
     sidebar: [
       {
         text: 'Introduction',
@@ -26,5 +45,12 @@ export default {
       pattern: 'https://github.com/northerjs/docs/edit/main/docs/:path',
       text: 'Contribute to this page',
     },
-  }
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022 Dominik Rajkowski'
+    },
+  },
+  markdown: {
+    theme: 'one-dark-pro',
+  },
 }
---
+++ index.md
@@ -0,0 +1,27 @@
+---
+layout: home
+
+hero:
+  text: Declarative backend framework for Node.js.
+  tagline: Create fast and secure web applications with awesome developer experience.
+  image:
+    src: /logo.png
+    alt: Norther Logo
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /docs/introduction/getting-started.html
+    - theme: alt
+      text: Visit GitHub
+      link: https://github.com/northerjs/norther
+features:
+  - icon: 🧩
+    title: Declarative
+    details: Build awesome web apps with a strong architecture and scalability.
+  - icon: 🤵
+    title: Elegant
+    details: Elegant and simple syntax which scales well in large projects.
+  - icon: ⚡️
+    title: Performant
+    details: Don't worry about performance.
+---
