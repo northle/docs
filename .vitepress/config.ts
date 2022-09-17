0547478ede00119c244f4b5383e54243d0e42809
Doc077 <dom.rajkowski@gmail.com>
2022-09-17 18:20:42 +0200

    Improve config and warning block

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -16,8 +16,14 @@ export default {
     logo: '/logo.png',
     outlineTitle: 'Table of Contents',
     nav: [
-      { text: 'Docs', link: '/docs/introduction/northerjs-framework.html' },
-      { text: 'Github', link: 'https://github.com/northerjs/norther' },
+      {
+        text: 'Docs',
+        link: '/docs/introduction/northerjs-framework.html',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/northerjs/norther',
+      },
     ],
     socialLinks: [
       {
@@ -29,33 +35,75 @@ export default {
       {
         text: 'Introduction',
         items: [
-          { text: 'NortherJS Framework', link: '/docs/introduction/northerjs-framework.html' },
-          { text: 'Getting Started', link: '/docs/introduction/getting-started.html' },
-        ]
+          {
+            text: 'NortherJS Framework',
+            link: '/docs/introduction/northerjs-framework.html',
+          },
+          {
+            text: 'Getting Started',
+            link: '/docs/introduction/getting-started.html',
+          },
+        ],
       },
       {
         text: 'Basics',
         items: [
-          { text: 'Directory Structure', link: '/docs/basics/directory-structure.html' },
-          { text: 'Configuration', link: '/docs/basics/configuration.html' },
-          { text: 'Controllers and Routing', link: '/docs/basics/controllers-and-routing.html' },
-          { text: 'Views', link: '/docs/basics/views.html' },
-          { text: 'Dependency Injection', link: '/docs/basics/dependency-injection.html' },
-          { text: 'Mail', link: '/docs/basics/mail.html' },
-        ]
+          {
+            text: 'Directory Structure',
+            link: '/docs/basics/directory-structure.html',
+          },
+          {
+            text: 'Configuration',
+            link: '/docs/basics/configuration.html',
+          },
+          {
+            text: 'Controllers and Routing',
+            link: '/docs/basics/controllers-and-routing.html',
+          },
+          {
+            text: 'Views',
+            link: '/docs/basics/views.html',
+          },
+          {
+            text: 'Dependency Injection',
+            link: '/docs/basics/dependency-injection.html',
+          },
+          {
+            text: 'Mail',
+            link: '/docs/basics/mail.html',
+          },
+        ],
       },
       {
         text: 'Advanced',
         items: [
-          { text: 'Localization', link: '/docs/advanced/localization.html' },
-          { text: 'Encryption and Hashing', link: '/docs/advanced/encryption-and-hashing.html' },
-        ]
+          {
+            text: 'Localization',
+            link: '/docs/advanced/localization.html',
+          },
+          {
+            text: 'Encryption and Hashing',
+            link: '/docs/advanced/encryption-and-hashing.html',
+          },
+        ],
+      },
+      {
+        text: 'Database',
+        items: [
+          {
+            text: 'Schema',
+            link: '/docs/database/schema.html',
+          },
+        ],
       },
       {
         text: 'Deployment',
         items: [
-          { text: 'App Deployment', link: '/docs/deployment/app-deployment.html' },
-        ]
+          {
+            text: 'App Deployment',
+            link: '/docs/deployment/app-deployment.html',
+          },
+        ],
       },
     ],
     editLink: {
--- docs/introduction/northerjs-framework.md
+++ docs/introduction/northerjs-framework.md
@@ -7,7 +7,7 @@ title: NortherJS Framework
 Norther is a modern, declarative backend development framework for Node.js. It makes it easy to create fast and secure web applications with awesome developer experience.
 
 ::: warning
-Norther is currently in `alpha` stage. You can encounter some bugs and API breaking changes.
+Norther is currently in `alpha` stage. You can encounter some bugs and API breaking changes. The documentation isn't fully complete yet.
 :::
 
 ## Requirements
