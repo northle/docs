026eccd53cdd631534ef2f4741eadfad96c37ec4
Doc077 <dom.rajkowski@gmail.com>
2022-10-19 19:44:00 +0200

    Customize theme

--- src/.vitepress/components/Layout.vue
+++ src/.vitepress/components/Layout.vue
@@ -6,12 +6,6 @@
 
 <template>
   <Layout>
-    <template #layout-top>
-      <aside class="banner">
-        <p>Northle is currently in the alpha stage.</p>
-      </aside>
-    </template>
-
     <template #home-features-before>
       <div class="intro">
         <div class="container vp-doc">
@@ -25,46 +19,6 @@
 </template>
 
 <style>
-  .Layout {
-    padding-top: 32px;
-  }
-
-  .VPLocalNav {
-    top: 32px !important;
-  }
-
-  .VPContent {
-    padding-top: 82px !important;
-  }
-
-  .VPSidebar {
-    top: 32px !important;
-  }
-
-  .VPNavScreen {
-    top: 32px !important;
-  }
-
-  .VPSkipLink {
-    top: 48px !important;
-  }
-
-  .banner {
-    height: 32px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    background-image: linear-gradient(to right, #4c8ffa, #14a9ee);
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-weight: 500;
-    font-size: 14px;
-    z-index: 100;
-  }
-
   .intro {
     padding: 0 24px;
     margin-bottom: 30px;
@@ -82,20 +36,6 @@
   @media (min-width: 960px) {
     .intro {
       padding: 0 64px;
-    }
-  }
-
-  @media (min-width: 768px) {
-    .VPNav {
-      top: 32px !important;
-    }
-
-    .aside-container {
-      top: 42px !important;
-    }
-
-    .aside-content .spacer {
-      display: none !important;
     }
   }
 
